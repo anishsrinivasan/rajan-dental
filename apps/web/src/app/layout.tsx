ed057f63721bc6a5ea81e9b9c622002ef96badef
thezeeshann <info.zeeshann@gmail.com>
2026-03-06 20:07:35 +0530

    feat: Removed header

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,39 +1,33 @@
 import type { Metadata } from "next";
-
 import { Montserrat } from "next/font/google";
-
 import "../index.css";
-import Header from "@/components/header";
 import Providers from "@/components/providers";
 
 const montserrat = Montserrat({
-	variable: "--font-montserrat",
-	subsets: ["latin"],
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-	style: ["normal", "italic"],
-	display: "swap",
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "my-better-t-app",
-	description: "my-better-t-app",
+  title: "my-better-t-app",
+  description: "my-better-t-app",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={`${montserrat.variable} font-sans antialiased`}>
-				<Providers>
-					<div className="grid h-svh grid-rows-[auto_1fr]">
-						<Header />
-						{children}
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${montserrat.variable} font-sans antialiased`}>
+        <Providers>
+          <div className="grid h-svh grid-rows-[auto_1fr]">{children}</div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
